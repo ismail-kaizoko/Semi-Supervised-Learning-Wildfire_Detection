ed7295cd3cd2ad9f5b34889157e648f542acd55e
ismail hamdaoui <ismail.hamdaoui@telecom-paris.fr>
2025-02-08 01:44:26 +0100

    first commit

---
+++ baselineCNN.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from PIL import Image
+import os
+
+
+
+baseline = nn.Sequential(
+    nn.Conv2d(3, 32, kernel_size=3, padding=1), 
+    nn.BatchNorm2d(32),
+    nn.ReLU(),
+    nn.Dropout(0.3),
+    nn.MaxPool2d(2),
+    nn.Conv2d(32, 64, kernel_size=3, padding=1),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+    nn.Dropout(0.3),
+    nn.MaxPool2d(2),
+    nn.Conv2d(64, 128, kernel_size=3, padding=1),
+    nn.BatchNorm2d(128),
+    nn.ReLU(),
+    nn.Dropout(0.3),
+    nn.MaxPool2d(2),
+    nn.Flatten(),
+    nn.LazyLinear(128),
+    nn.ReLU(),
+    nn.Linear(128,2)
+)
