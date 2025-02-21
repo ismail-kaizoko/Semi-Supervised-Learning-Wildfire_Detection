f9ae79c86f866151337f43ef7af59d318bd97137
ismail hamdaoui <ismail.hamdaoui@telecom-paris.fr>
2025-02-21 10:06:39 +0100

    worked fine

--- baselineCNN.py
+++ baselineCNN.py
@@ -7,25 +7,35 @@ from PIL import Image
 import os
 
 
+import torch
+import torch.nn as nn
 
-baseline = nn.Sequential(
-    nn.Conv2d(3, 32, kernel_size=3, padding=1), 
-    nn.BatchNorm2d(32),
-    nn.ReLU(),
-    nn.Dropout(0.3),
-    nn.MaxPool2d(2),
-    nn.Conv2d(32, 64, kernel_size=3, padding=1),
-    nn.BatchNorm2d(64),
-    nn.ReLU(),
-    nn.Dropout(0.3),
-    nn.MaxPool2d(2),
-    nn.Conv2d(64, 128, kernel_size=3, padding=1),
-    nn.BatchNorm2d(128),
-    nn.ReLU(),
-    nn.Dropout(0.3),
-    nn.MaxPool2d(2),
-    nn.Flatten(),
-    nn.LazyLinear(128),
-    nn.ReLU(),
-    nn.Linear(128,2)
-)
+class BaselineModel(nn.Module):
+    def __init__(self):
+        super(BaselineModel, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1), 
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.LazyLinear(128),
+            nn.ReLU(),
+            nn.Linear(128, 2)
+        )
+        
+    def forward(self, x):
+        return self.model(x)
+
+
